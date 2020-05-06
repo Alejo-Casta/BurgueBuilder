08e7f11c789ea41c99befad0d38f0e8f7f39b22f
alejocastagit <alejo.casta.al@gmail.com>
2020-05-06 02:34:55 -0500

    125. Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Auxiliar from '../../hoc/Auxiliar'
 
+import style from './Layout.module.css'
+
 const Layout = (props) => (
     <Auxiliar>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={style.Content}>
             {props.children}
         </main>
     </Auxiliar>
 )
+
 export default Layout;
---
+++ src/containers/BurgerBuilder/BulgerBuilder.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Auxiliar from '../../hoc/Auxiliar'
+
+class BurgerBuilder extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <Auxiliar>
+                <div>Burguer</div>
+                <div>Build Controls</div>
+            </Auxiliar>
+
+        )
+    }
+}
+
+export default BurgerBuilder;
